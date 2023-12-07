c9afa91be0c34dba950239b2d54349e7aa92820e
Anderson <avictoral@gmail.com>
2023-12-06 22:36:45 -0500

    created script for product

---
+++ js/product.js
@@ -0,0 +1,25 @@
+const watches = [
+    {
+      id: 1,
+      name: "Classic Elegance",
+      description: "A timeless watch with a classic design, perfect for any occasion.",
+      price: 199.99,
+      image: "./images/Slider/Slider1.jpg",
+    },
+    {
+      id: 2,
+      name: "Sporty Chronograph",
+      description: "For the active individual, this sporty chronograph combines style and functionality.",
+      price: 249.99,
+      image: "./images/Slider/Slider4.jpg",
+    },
+    {
+      id: 3,
+      name: "Modern Minimalism",
+      description: "Embrace simplicity with this modern minimalist watch, a statement of contemporary style.",
+      price: 159.99,
+      image: "modern_minimalism.jpg",
+    },
+
+  ];
+  
